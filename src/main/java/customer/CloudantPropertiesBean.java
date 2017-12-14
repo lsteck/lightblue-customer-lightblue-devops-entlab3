dcd04038509fc795e2b1f53c2989f20d88ae106d
Larry Steck <lsteck@us.ibm.com>
2017-12-14 17:38:19 +0000

    Connect to Cloudant

--- src/main/java/customer/CloudantPropertiesBean.java
+++ src/main/java/customer/CloudantPropertiesBean.java
@@ -1,3 +1,5 @@
+package customer;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
--- src/main/java/customer/Customer.java
+++ src/main/java/customer/Customer.java
@@ -1,3 +1,5 @@
+package customer;
+
 public class Customer {
 	private String _id;
 	private String _rev;
