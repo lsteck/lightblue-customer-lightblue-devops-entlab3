f7a33eb45ff7aa2962c36c4929b32057b211ef12
Larry Steck <lsteck@us.ibm.com>
2017-12-14 00:21:05 +0000

    Connect to Cloudant

---
+++ src/main/java/customer/Customer.java
@@ -0,0 +1,60 @@
+public class Customer {
+	private String _id;
+	private String _rev;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String imageUrl;
+	
+	public String getId() {
+		return _id;
+	}
+	public void setId(String id) {
+		this._id = id
+	}
+	public String getRev() {
+		return _rev;
+	}
+	public void setRev(String rev) {
+		this._rev = rev
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+}
